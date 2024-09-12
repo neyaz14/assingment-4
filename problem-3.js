8d7a5ce45100be4153d9c023aeaec9270ed3bf76
neyaz14 <neyazmorshid@gmail.com>
2024-09-12 16:18:36 +0600

    all problem done

--- problem-3.js
+++ problem-3.js
@@ -1,6 +1,16 @@
 function checkDigitsInName(name) {
-    const num = [0,1,2,3,4,5,6,7,8,9];
+    if(typeof name !== 'string'){
+        return 'Invalid Input';
+    }
+    const num = ['0','1','2','3','4','5','6','7','8','9'];
+    for(let i=0; i<num.length; i++){
+        if(name.includes(num[i])== true){
+            return true;
+        }
+    }
+    return false;
+   
+   
     
 }
 
-console.log(checkDigitsInName("R1"));
